342dc1bf2dd007521dc79fbaf68dcc89112e3a12
许磊 <xulei131401@outlook.com>
2023-09-18 16:50:32 +0800

    fix

--- packages/learn-typescript/guide/src/interface/interface.ts
+++ packages/learn-typescript/guide/src/interface/interface.ts
@@ -1,17 +1,14 @@
-// ts接口
+/**
+ *  学习interface
+ */
 
-// 不是对象实现接口，而是约束对象必须包含接口定义的结构
-
-
-// color?: string 可选
-//readonly x: number; 只读
-interface LabelledValue {
-  label: string;
+export interface Student {
+    name:string
 }
 
-function printLabel(labelledObj: LabelledValue) {
-  console.log(labelledObj.label);
+let defaultStu = {
+    name: "默认学生"
 }
 
-let myObj = { size: 10, label: "Size 10 Object" };
-printLabel(myObj);
+
+console.log("defaultStu:", defaultStu);
